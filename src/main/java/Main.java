384ee6a0a4884eead7e49abbdbf378e97b23d2a6
Victor Fabris <victor.fabris.santos@escola.pr.gov.br>
2024-05-27 12:35:13 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa para calcular a velocidade média em km/h");
+    System.out.println("");
+    System.out.println("A distância considerada é:");
+    double distancia = 500;
+    System.out.println(distancia);
+    System.out.println("o tempo gasto é");
+    double tempo = 7;
+    System.out.println(tempo);
+    System.out.println("A velocidade média é:");
+    double velocidadeMedia = distancia / tempo;
+    System.out.println(velocidadeMedia);
+  } 
+}
